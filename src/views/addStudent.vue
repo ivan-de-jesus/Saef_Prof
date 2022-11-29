cb84d4dfd6a615bb3317619b7a8c79ccc067c4f5
ivan-de-jesus <ian_clan95@hotmail.com>
2022-11-29 09:43:11 -0600

    ultimo

--- src/views/addStudent.vue
+++ src/views/addStudent.vue
@@ -1,244 +1,269 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="desserts"
-    sort-by="albumNumerus"
-    class="elevation-1"
-  >
+<v-data-table :headers="headers" :items="desserts" sort-by="albumNumerus" class="elevation-1">
     <template v-slot:top>
-      <v-toolbar flat>
-        <v-toolbar-title>Alumnos registrados</v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
-        <v-spacer></v-spacer>
-        <v-dialog v-model="dialog" max-width="500px">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              color="primary"
-              dark
-              class="mb-2 pl-6"
-              v-bind="attrs"
-              v-on="on"
-            >
-              Nuevo alumno
-            </v-btn>
-            <v-btn
-              color="warning"
-              dark
-              class="mb-2 pr-6"
-              v-bind="attrs"
-              v-on="on"
-            >
-              Asignar ejercicios
-            </v-btn>
-          </template>
+        <v-toolbar flat>
+            <v-toolbar-title>Alumnos registrados</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialog" max-width="500px">
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn color="primary" dark class="mb-2 pl-6" v-bind="attrs" v-on="on">
+                        Nuevo alumno
+                    </v-btn>
+                    <v-btn color="warning" @click="toAssignItem" dark class="mb-2 pr-6">
+                        Asignar ejercicios
+                    </v-btn>
+                </template>
 
-          <v-card>
-            <v-card-title>
-              <span class="text-h5">{{ formTitle }}</span>
-            </v-card-title>
+                <v-card>
+                    <v-card-title>
+                        <span class="text-h5">{{ formTitle }}</span>
+                    </v-card-title>
 
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.name"
-                      label="Nombre del alumno"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.albumNumerus"
-                      label="No.Lista"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.gradus"
-                      label="Grado"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.group"
-                      label="Grupo"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.age"
-                      label="Edad"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.pass"
-                      label="Contraseña"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
+                    <v-card-text>
+                        <v-container>
+                            <v-row>
+                                <v-col cols="12" sm="6" md="4">
+                                    <v-text-field v-model="editedItem.name" label="Nombre del alumno"></v-text-field>
+                                </v-col>
+                                <v-col cols="12" sm="6" md="4">
+                                    <v-text-field v-model="editedItem.albumNumerus" label="No.Lista"></v-text-field>
+                                </v-col>
+                                <v-col cols="12" sm="6" md="4">
+                                    <v-text-field v-model="editedItem.gradus" label="Grado"></v-text-field>
+                                </v-col>
+                                <v-col cols="12" sm="6" md="4">
+                                    <v-text-field v-model="editedItem.group" label="Grupo"></v-text-field>
+                                </v-col>
+                                <v-col cols="12" sm="6" md="4">
+                                    <v-text-field v-model="editedItem.age" label="Edad"></v-text-field>
+                                </v-col>
+                                <v-col cols="12" sm="6" md="4">
+                                    <v-text-field v-model="editedItem.pass" label="Contraseña"></v-text-field>
+                                </v-col>
+                            </v-row>
+                        </v-container>
+                    </v-card-text>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="close"> Cancelar </v-btn>
-              <v-btn color="blue darken-1" text @click="save"> Guardar </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title class="text-h5"
-              >Are you sure you want to delete this item?</v-card-title
-            >
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete"
-                >Cancel</v-btn
-              >
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm"
-                >OK</v-btn
-              >
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-toolbar>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" text @click="close"> Cancelar </v-btn>
+                        <v-btn color="blue darken-1" text @click="save"> Guardar </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+            <v-dialog v-model="dialogDelete" max-width="500px">
+
+                <v-card>
+                    <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                        <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                        <v-spacer></v-spacer>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+
+            <v-dialog v-model="dialogToassign" max-width="500px">
+
+                <v-card>
+                    <v-card-title class="text-h5">Se asignarán los ejercicios a todos los estudiantes de la lista </v-card-title>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" text @click="closeToassign">Cancel</v-btn>
+                        <v-btn color="blue darken-1" text @click="assingChange">OK</v-btn>
+                        <v-spacer></v-spacer>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-toolbar>
     </template>
     <template v-slot:item.actions="{ item }">
-      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
-      <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+        <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+        <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+        <v-icon v-if="AssignIcon" small class="ms-2"> mdi-clipboard-text </v-icon>
     </template>
     <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize"> Reset </v-btn>
+        <v-btn color="primary" @click="initialize"> Reset </v-btn>
     </template>
-  </v-data-table>
+</v-data-table>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    dialog: false,
-    dialogDelete: false,
-    headers: [
-      {
-        text: "Nombre del alumno",
-        align: "start",
-        sortable: false,
-        value: "name",
-      },
-      { text: "No. Lista", value: "albumNumerus" },
-      { text: "Grado", value: "gradus" },
-      { text: "Grupo", value: "group" },
-      { text: "Edad", value: "age" },
-      { text: "Contraseña", value: "pass" },
-      { text: "Accion", value: "actions", sortable: false },
-    ],
-    desserts: [],
-    editedIndex: -1,
-    editedItem: {
-      name: "",
-      albumNumerus: 1,
-      gradus: 0,
-      group: 0,
-      age: 0,
-    },
-    defaultItem: {
-      name: "",
-      albumNumerus: 0,
-      gradus: 0,
-      group: 0,
-      age: 0,
-    },
-    idDelete:null,
-  }),
-  computed: {
-    formTitle() {
-      return this.editedIndex === -1 ? "Agregar alumno" : "Editar alumno";
-    },
-  },
-  watch: {
-    dialog(val) {
-      val || this.close();
-    },
-    dialogDelete(val) {
-      val || this.closeDelete();
-    },
-  },
-  created() {
-    this.initialize();
-    this.getStudents();
-  },
-  methods: {
-    initialize() {
-      this.desserts = [];
-    },
-    async getStudents() {
-      const res = await this.axios.get("http://localhost:3000/students");
-      console.log(res);
-      this.desserts = res.data;
-    },
-    editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
-    },
-    deleteItem(item) {
-      console.log("Item seleccionado",item._id);
-      this.idDelete = item._id;
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialogDelete = true;
-    },
-    deleteItemConfirm() {
-      this.desserts.splice(this.editedIndex, 1);
-      this.closeDelete();
-      let url = "http://localhost:3000/delete/" + this.idDelete;
-      const res =  this.axios.get(url);
-      
-    },
-    close() {
-      this.dialog = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
-    },
-    closeDelete() {
-      this.dialogDelete = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
-    },
-    save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem);
-      } else {
-        this.desserts.push(this.editedItem);
-        this.axios
-          .post("http://localhost:3000/create",this.editedItem,
+    data: () => ({
+        dialog: false,
+        dialogDelete: false,
+        dialogToassign: false,
+        AssignIcon: false,
+        headers: [{
+                text: "Nombre del alumno",
+                align: "start",
+                sortable: false,
+                value: "name",
+            },
             {
-              params: {
-                name: this.editedItem.name,
-                albumNumerus: this.editedItem.albumNumerus,
-                gradus: this.editedItem.gradus,
-                group: this.editedItem.group,
-                age: this.editedItem.age,
-                pass: this.editedItem.pass
-              },
-            }
-          )
-          .then((res) => {
-            console.log(res);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
-      this.close();
+                text: "No. Lista",
+                value: "albumNumerus"
+            },
+            {
+                text: "Grado",
+                value: "gradus"
+            },
+            {
+                text: "Grupo",
+                value: "group"
+            },
+            {
+                text: "Edad",
+                value: "age"
+            },
+            {
+                text: "Contraseña",
+                value: "pass"
+            },
+            {
+                text: "Accion",
+                value: "actions",
+                sortable: false
+            },
+        ],
+        desserts: [],
+        editedIndex: -1,
+        editedItem: {
+            name: "",
+            albumNumerus: 1,
+            gradus: 0,
+            group: 0,
+            age: 0,
+        },
+        defaultItem: {
+            name: "",
+            albumNumerus: 0,
+            gradus: 0,
+            group: 0,
+            age: 0,
+        },
+        idDelete: null,
+    }),
+    computed: {
+        formTitle() {
+            return this.editedIndex === -1 ? "Agregar alumno" : "Editar alumno";
+        },
     },
-  },
+    watch: {
+        dialog(val) {
+            val || this.close();
+        },
+        dialogDelete(val) {
+            val || this.closeDelete();
+        },
+        dialogToassign(val) {
+            val || this.closeToassign();
+        },
+    },
+    created() {
+        this.initialize();
+        this.getStudents();
+        this.getExercises();
+    },
+    methods: {
+        initialize() {
+            this.desserts = [];
+        },
+        async getStudents() {
+            const res = await this.axios.get("http://localhost:3000/students");
+            console.log("jjj", res);
+            this.desserts = res.data;
+        },
+        async getExercises() {
+            const res = await this.axios.get("http://localhost:3000/exercises");
+            this.exercises = res.data;
+            console.log("Ejercicios", this.exercises);
+        },
+        editItem(item) {
+            this.editedIndex = this.desserts.indexOf(item);
+            this.editedItem = Object.assign({}, item);
+            this.dialog = true;
+        },
+        deleteItem(item) {
+            console.log("Item seleccionado", item._id);
+            this.idDelete = item._id;
+            this.editedIndex = this.desserts.indexOf(item);
+            this.editedItem = Object.assign({}, item);
+            this.dialogDelete = true;
+        },
+        toAssignItem() {
+
+            this.dialogToassign = true;
+        },
+
+        assingChange() {
+            this.dialogToassign = false;
+
+            console.log("items en cambio de asignacion", this.desserts)
+
+            const cambiarAsignacion = this.desserts.map(function (ejercicio) {
+                return ejercicio.is_good = true;
+            });
+
+            console.log(cambiarAsignacion);
+
+        },
+
+        deleteItemConfirm() {
+            this.desserts.splice(this.editedIndex, 1);
+            this.closeDelete();
+            let url = "http://localhost:3000/delete/" + this.idDelete;
+            const res = this.axios.get(url);
+
+        },
+
+        close() {
+            this.dialog = false;
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem);
+                this.editedIndex = -1;
+            });
+        },
+        closeDelete() {
+            this.dialogDelete = false;
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem);
+                this.editedIndex = -1;
+            });
+        },
+        closeToassign() {
+            this.dialogToassign = false;
+
+        },
+        save() {
+            if (this.editedIndex > -1) {
+                Object.assign(this.desserts[this.editedIndex], this.editedItem);
+            } else {
+                this.desserts.push(this.editedItem);
+                this.axios
+                    .post("http://localhost:3000/create", this.editedItem, {
+                        params: {
+                            name: this.editedItem.name,
+                            albumNumerus: this.editedItem.albumNumerus,
+                            gradus: this.editedItem.gradus,
+                            group: this.editedItem.group,
+                            age: this.editedItem.age,
+                            pass: this.editedItem.pass,
+                            assignedExercise: true
+                        },
+                    })
+                    .then((res) => {
+                        console.log(res);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            }
+            this.close();
+        },
+    },
 };
 </script>
--- src/views/createExercises.vue
+++ src/views/createExercises.vue
@@ -1,70 +1,82 @@
 <template>
-  <div>
+<div>
     <v-row align="center">
-      <v-col cols="2" md="2">
-        <v-textarea  width="20" solo v-model="numerador" name="input-7-4" label="Solo textarea"></v-textarea>
-        <hr width="230">
-        </hr>
+        <v-col cols="2" md="2">
+            <v-textarea width="20" solo v-model="numerador" name="input-7-4" label="Solo textarea"></v-textarea>
+            <hr width="230">
+            </hr>
 
-        <v-textarea class="pt-3" solo v-model="denominador" name="input-7-4" label="Solo textarea"></v-textarea>
-      </v-col>
-      <v-col cols="1" md="1">
-        <v-autocomplete v-model="signo" :items="items" outlined dense label="Outlined"></v-autocomplete>
-      </v-col>
-      <v-col cols="2" md="2">
-        <v-textarea solo v-model="numerador2" name="input-7-4" label="Solo textarea"></v-textarea>
-        <hr width="230">
-        </hr>
+            <v-textarea class="pt-3" solo v-model="denominador" name="input-7-4" label="Solo textarea"></v-textarea>
+        </v-col>
+        <v-col cols="1" md="1">
+            <v-autocomplete v-model="signo" :items="items" outlined dense label="Outlined"></v-autocomplete>
+        </v-col>
+        <v-col cols="2" md="2">
+            <v-textarea solo v-model="numerador2" name="input-7-4" label="Solo textarea"></v-textarea>
+            <hr width="230">
+            </hr>
 
-        <v-textarea class="pt-3" solo v-model="denominador2" name="input-7-4" label="Solo textarea"></v-textarea>
-      </v-col>
-      <v-col>
-        <template>
-        <v-row>
-          
-       
-          <v-col cols="4" md="4">
-        <v-textarea solo v-model="instruction" name="input-7-4" label="Instrucciones"></v-textarea>
-      <v-textarea solo v-model="help" name="input-7-4" label="Ayuda"></v-textarea>
-      </v-col>
+            <v-textarea class="pt-3" solo v-model="denominador2" name="input-7-4" label="Solo textarea"></v-textarea>
+        </v-col>
+        <v-col cols="1" md="1">
+          <v-text-field
+            label="="
+            placeholder="="
+            filled
+            disabled
+            center
+          ></v-text-field>
+        </v-col>
+        <v-col cols="2" md="2">
+            <v-textarea solo v-model="respuestaNumerador" name="input-7-4" label="Respuesta"></v-textarea>
+            <hr width="230">
+            </hr>
 
-          <v-col cols="6" md="6">
-            <v-timeline dense clipped>
-            <v-slide-x-transition group>
-            </v-slide-x-transition>
+            <v-textarea class="pt-3" solo v-model="respuestaDenominador" name="input-7-4" label="Respuesta"></v-textarea>
+        </v-col>
+        <v-col>
 
-            <v-timeline-item class="mb-6" hide-dot>
-              <span>TODAY</span>
-            </v-timeline-item>
+            <template>
+                <v-row>
 
-            <v-timeline-item v-for="(fraction, i) in fractions" :key="i" fill-dot>
-              <v-row justify="space-between">
-                <v-col cols="6">
-                  <span v-text="fraction.exercise"></span>
+                    <v-col cols="6" md="6">
+                        <v-timeline dense clipped>
+                            <v-slide-x-transition group>
+                            </v-slide-x-transition>
 
-                </v-col>
-             
-                <v-col class="text-right" cols="2">
-                  <v-icon small class="mr-2"> mdi-pencil </v-icon>
-                  <v-icon small  @click="deleteItem(item)"> mdi-delete </v-icon>
-                </v-col>
-              </v-row>
-            </v-timeline-item>
-          </v-timeline>
-          
-          </v-col>
-        </v-row>
+                            <v-timeline-item class="mb-6" hide-dot>
+                                <span>Ejercicios</span>
+                            </v-timeline-item>
 
-   
-        </template>
-        
-      </v-col>
+                            <v-timeline-item v-for="(fraction, i) in fractions" :key="i" fill-dot>
+                                <v-row justify="space-between">
+                                    <v-col cols="6">
+                                        <span v-text="fraction.exercise"></span>
+
+                                    </v-col>
+                                    <v-col class="text-right" cols="2">
+                                        <v-icon small class="mr-2"> mdi-pencil </v-icon>
+                                        <v-icon small @click="deleteItem(fraction._id)"> mdi-delete </v-icon>
+                                    </v-col>
+                                </v-row>
+                            </v-timeline-item>
+                        </v-timeline>
+                    </v-col>
+                </v-row>
+
+            </template>
+        </v-col>
+
     </v-row>
-   
-    <v-btn text class="success">
-      <span class="mr-2" @click="click">Guardar</span>
-    </v-btn>
-  </div>
+    <v-col cols="4" md="4">
+        <v-textarea solo v-model="instruction" name="input-7-4" label="Instrucciones"></v-textarea>
+        <v-textarea solo v-model="help" name="input-7-4" label="Ayuda"></v-textarea>
+        <v-btn text class="success">
+            <span class="mr-2" @click="click">Guardar</span>
+        </v-btn>
+    </v-col>
+
+</div>
 </template>
 
 <script>
@@ -73,88 +85,94 @@ import Card from "@/components/Card.vue";
 import Ranking from "../components/Ranking.vue";
 
 export default {
-  name: "dashboard",
-  components: {
-    Card,
-    Ranking,
-  },
-
-  data: () => ({
-
-    items: ['+', '-', '÷', 'x'],
-    numerador: "",
-    denominador: "",
-    numerador2: "",
-    denominador2: "",
-    signo: "",
-    instruction: "",
-    help: "",
-    fractions: "",
-
-    events: [],
-    input: null,
-    nonce: 0,
-
-  }),
-
-  created() {
-
-    this.getExercises();
-  },
-
-  computed: {
-    timeline() {
-      return this.events.slice().reverse()
-    },
-  },
-
-  methods: {
-    
-
-
-    click() {
-
-      let fraccion = this.numerador + "/" + this.denominador + "" + this.signo + "" + this.numerador2 + "/" + this.denominador2
-      console.log("Se preciono el boton", fraccion)
-      let number = 1;
-
-      let exercise = {
-        fraccion: fraccion,
-        number: number,
-        instruction: this.instruction,
-        help: this.help
-      };
-      
-
-      this.axios
-        .post("http://localhost:3000/addExercises", exercise,
-          {
-            params: {
-              number: exercise.number,
-              exercise: exercise.fraccion,
-              instruction: exercise.instruction,
-              help: exercise.help
-            },
-          }
-        )
-        .then((res) => {
-          console.log(res);
-          this.getExercises()
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-
-    },
-    deleteItem(item) {
-      //
-    },
-    async getExercises() {
-      const res = await this.axios.get("http://localhost:3000/exercises");
-      console.log(res.data)
-      this.fractions = res.data
+    name: "dashboard",
+    components: {
+        Card,
+        Ranking,
     },
 
-  },
+    data: () => ({
+        a: {},
+        b: {},
+
+        items: ['+', '-', '÷', 'x'],
+        numerador: "",
+        denominador: "",
+        numerador2: "",
+        denominador2: "",
+        respuestaNumerador: "",
+        respuestaDenominador:"",
+        signo: "",
+        instruction: "",
+        help: "",
+        fractions: "",
+
+        events: [],
+        input: null,
+        nonce: 0,
+
+    }),
+
+    created() {
+
+        this.getExercises();
+    },
+
+    computed: {
+        timeline() {
+            return this.events.slice().reverse()
+        },
+    },
+
+    methods: {
+
+        click() {
+
+            let fraccion = this.numerador + "/" + this.denominador + "" + this.signo + "" + this.numerador2 + "/" + this.denominador2;
+            let number = 1;
+            let resultado = this.respuestaNumerador + "/" + this.respuestaDenominador;
+            
+            let exercise = {
+                fraccion: fraccion,
+                result: resultado,
+                number: number,
+                instruction: this.instruction,
+                help: this.help
+            };
+            console.log("Se preciono el boton RESULTADO", exercise)
+
+            this.axios
+                .post("http://localhost:3000/addExercises", exercise, {
+                    params: {
+                        number: exercise.number,
+                        exercise: exercise.fraccion,
+                        result: exercise.result ,
+                        instruction: exercise.instruction,
+                        help: exercise.help,
+                        status: false,
+                        is_good: false
+                    },
+                })
+                .then((res) => {
+                    console.log(res);
+                    this.getExercises()
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+
+        },
+        deleteItem(item) {
+            console.log("Item del ejercicio", item)
+            let url = "http://localhost:3000/deleteExercise/" + item;
+            const res = this.axios.get(url);
+        },
+        async getExercises() {
+            const res = await this.axios.get("http://localhost:3000/exercises");
+            console.log(res.data)
+            this.fractions = res.data
+        },
+
+    },
 };
 </script>
